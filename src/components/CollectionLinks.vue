2039e78997460aa66a87d0025ebc417388a66f25
Carlos Alvarez Linares <alvarez15755@iesmarenostrum.com>
2019-02-06 17:36:51 +0100

    Carlos Index

--- src/components/CollectionLinks.vue
+++ src/components/CollectionLinks.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container">
         <a class="navbar-brand" href="/">
           <img src="https://getbootstrap.com/docs/4.2/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
