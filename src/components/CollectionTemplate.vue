1d3fbd51014780e79a8359e4c5040a28d0723bbe
carlitosalli <carlitosalvarezlinares@gmail.com>
2019-02-15 20:41:16 +0100

    Carlos loader

--- src/components/CollectionTemplate.vue
+++ src/components/CollectionTemplate.vue
@@ -2,9 +2,11 @@
   <!-- This component will build a web form to display the collection.template available in the 'template' prop. This form will be bound to the 'template' prop. When the form is submitted, the method 'createItem' will be called to send the data to the server and create a new item. -->
   <!-- How to bind form inputs to data: https://vuejs.org/v2/guide/forms.html -->
   <!-- Listening to events: https://vuejs.org/v2/guide/events.html -->
-  <div class="container">
-      <button class="btn btn-secondary m-1 btn-sm" v-on:click="activo=!activo">+</button>
-      <div v-show="activo" class="card p-2 m-2">
+
+  <!--ESTE FUNCIONA-->
+  <!--<div class="container">
+    <button class="btn btn-secondary m-1 btn-sm" v-on:click="activo=!activo">+</button>
+    <div v-show="activo" class="card p-2 m-2">
       <div class="form-group" v-for="item in template.data">
         <div class="row">
           <div class="col-md-3">
@@ -19,6 +21,41 @@
       <div class="row">
         <div class="col-12">
           <button class="btn btn-success btn-sm" v-on:click="createItem(item)">Crear Nuevo</button>
+        </div>
+      </div>
+    </div>
+  </div>-->
+
+  <div class="container">
+    <button class="btn btn-secondary m-1 btn-sm" v-on:click="activo=!activo"  data-toggle="modal" data-target="#modalNew">+</button>
+    <div class="modal fade bd-example-modal-lg" id="modalNew" tabindex="-1" role="dialog" aria-labelledby="modalNewLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4>Crear un nuevo item</h4>
+            <button type="button" class="close bg-danger" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div v-show="activo" class="card p-2 m-2 sinColor">
+              <div class="form-group" v-for="item in template.data">
+                <div class="row">
+                  <div class="col-md-3">
+                    <label>{{item.prompt}}: </label>
+                  </div>
+                  <div class="col-md">
+                    <input class="form-control" v-model="item.value" :type="item.type" :name="item.name" v-if="item.type!='textarea'" :placeholder="item.prompt + '...'">
+                    <textarea class="form-control" v-model="item.value" :type="item.type" :name="item.name" v-if="item.type=='textarea'" :placeholder="item.prompt + '...'" rows="6"></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            <button class="btn btn-success btn-md" v-on:click="createItem(item)">Crear Nuevo</button>
+          </div>
         </div>
       </div>
     </div>
